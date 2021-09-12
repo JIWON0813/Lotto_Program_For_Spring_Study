92be8e0cc1591393b3f50d4988094c854b1f7bff
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2021-09-13 00:02:42 +0900

    확장메서드 구현

---
+++ src/main/java/persnal/practice/lotto/commons/core/StringExtension.java
@@ -0,0 +1,16 @@
+package persnal.practice.lotto.commons.core;
+
+//확장메소드 구현하려 했으나 manifold 플러그인이 현재 IDE버전과 맞지않음
+//import manifold.ext.api.*;
+//
+//@Extension
+public class StringExtension {
+//    public static void print(@This String thiz) {
+//        System.out.println(thiz);
+//    }
+//
+//    @Extension
+//    public static String lineSeparator() {
+//        return System.lineSeparator();
+//    }
+}
